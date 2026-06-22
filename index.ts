1ea9e15cc2d3c59188b418cbed16c1aaf2a5ab85
Sankalp Srivastava <srivastavasankalp509@gmail.com>
2026-06-22 21:40:52 +0530

    phase 1 commit

--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
-console.log("Hello via Bun!");
+#!/usr/bin/env bun 
+import { log } from "@clack/prompts";
+import { Command } from "commander";
+const program = new Command();
+program
+        .name("Khulja_Sim_Sim")
+        .description("Khulja_Sim_Sim cli yt")
+        .version("0.0.1");
+
+program
+    .command("wakeup")
+    .description("Show the Banner and picj cli or telegram mode")
+    .action(
+        async()=>{
+            console.log("WakrUp calling ....")
+        }
+    );
+
+    await program.parseAsync(process.argv);
